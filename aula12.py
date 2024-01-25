2043a4219073357e4200a545a1b0d31d293321ad
Vinicius <soaresvinicius11112@gmail.com>
2024-01-25 02:14:50 -0300

    Enviando aulas 6 até 13

---
+++ aula12.py
@@ -0,0 +1,10 @@
+nome = 'Luiz Otávio'
+altura = float(1.80)
+peso = 95
+imc = peso / altura ** 2  # IMC =  peso / (altura x altura)
+print(nome, 'tem', altura, 'de altura', ',', 'pesa', peso, 'quilos',"e seu IMC é:", imc)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é 
+# 29.320987654320987
+
---
+++ aula7.py
@@ -0,0 +1,17 @@
+# Variáveis são usadas para salva algo na memória do computador.
+# PEP8: inicie variáveis com lestras minúsculas, pode usar 
+# números e underline _.
+# O sinal de =  é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_varialvel = expressão
+# nome_completo= 'Vinicius Gomes Soares'
+# soma_dois_mais_dois = 2 + 2 
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois) 
+
+nome = 'Vinicius'
+idade = 25
+maior_de_idade = idade >= 18
+print ('Nome:', nome, 'Idade:', idade)
+print ('É maior?', maior_de_idade)
